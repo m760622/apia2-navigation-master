91e15f71d8e421b91741e7e42c967d01b1b8cd3c
Mohammed Abunada <m7606225@gmail.com>
2018-10-14 12:57:23 +0200

    drawerLayout error

--- app/src/main/java/com/rakangsoftware/navigation/screen/NavigationActivity.kt
+++ app/src/main/java/com/rakangsoftware/navigation/screen/NavigationActivity.kt
@@ -1,12 +1,13 @@
 package com.rakangsoftware.navigation.screen
 
-import android.content.Context
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.NavigationView
 import android.support.v4.app.NavUtils
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
+import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import com.rakangsoftware.navigation.R
@@ -14,11 +15,35 @@ import kotlinx.android.synthetic.main.navigation_activity.*
 
 class NavigationActivity : AppCompatActivity() {
 
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.navigation_activity)
+        val drawerLayout = findViewById(R.id.drawer_layout)
 
+        val navigationView: NavigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.local_bar -> {
+                    nav_label.text = getString(R.string.bottom_nav_bar)
+                    drawer_layout.closeDrawer(Gravity.START)
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        drawerToggle = ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        )
+        drawer_layout.addDrawerListener(drawerToggle)
 
 
 
@@ -51,21 +76,13 @@ class NavigationActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-      //  val intent = Intent(this, SecondActivity::class.java)
-      //  startActivity(intent)
-
         when (item.itemId) {
             R.id.shareMenu -> nav_label.text = "Toolbar bar nav pressed."
             R.id.settingsMenu -> nav_label.text = "Toolbar Settings nav pressed."
             R.id.CloseMenu -> finish()
         //    android.R.id.home -> onBackPressed()
           //  R.id.home ->  onBackPressed()
-
-            R.id.local_florist -> {
-              //  val intent=Intent(applicationContext, SecondActivity::class.java)
-              //      startActivity(intent)
-                SecondActivity.start(this)
-            }
+            R.id.local_florist -> SecondActivity.start(this)
             android.R.id.home -> {
                 NavUtils.navigateUpFromSameTask(this)
                 return true
@@ -73,6 +90,13 @@ class NavigationActivity : AppCompatActivity() {
         }
      //   return true
         return super.onOptionsItemSelected(item)
+    }
+
+
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
     }
 
 
